2322756c8daad220a231f11da5176bc26bb6e501
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-06-08 17:38:26 +0530

    code updated

---
+++ College-Event-Management-System-master/classes/Untitled-1.php
@@ -0,0 +1,11 @@
+<!-- <?php
+$servername = "localhost:4306";
+$username = "ashu";
+$password = "";
+$dbname = "cemss";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+} 
+?> -->
